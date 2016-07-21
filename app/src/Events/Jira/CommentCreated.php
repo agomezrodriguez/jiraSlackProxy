69001c68de89bf4e8aa6bf02e98e9d3c7a5eefdf
Alberto Gómez <alberto.gomez@interactiv4.com>
2016-07-21 14:06:44 +0200

    minor change

--- app/src/Events/Jira/CommentCreated.php
+++ app/src/Events/Jira/CommentCreated.php
@@ -29,7 +29,7 @@ class CommentCreated implements JiraTriggerInterface
         $data = $request->getParsedBody();
         $jiraBaseUrl = $this->config['jiraBaseUrl'];
         $commentLink = $jiraBaseUrl . '/browse/' . $queryParams['issue_key'] .'#comment-' . $queryParams['comment_id'];
-        $addLink = $jiraBaseUrl . 'https://interactiv4.atlassian.net/browse/' . $queryParams['issue_key'] .'#add-comment';
+        $addLink = $jiraBaseUrl . '/browse/' . $queryParams['issue_key'] .'#add-comment';
         $message = "*" . $data['comment']['author']['displayName'] . "* commented on <" . $commentLink . "|" . $queryParams['issue_key'] . ">\n\n ```" .
             $data['comment']['body'] . "``` \n\n <" . $addLink . "|Add comment>";
         $slackResponse = $this->httpClientAbstract->postToSlack($request, $message);
